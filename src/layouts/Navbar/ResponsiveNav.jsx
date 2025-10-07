9728259137907d2174436f52af2618250848f69a
Sheik1419 <sheikgamer567@gmail.com>
2025-10-07 09:05:40 +0530

    changes

--- src/layouts/Navbar/ResponsiveNav.jsx
+++ src/layouts/Navbar/ResponsiveNav.jsx
@@ -8,25 +8,25 @@ const ResponsiveNav = () => {
       <div className="responsiveNav-container">
         <NavLink to="/" className="respnav-item">
           <button className="respnavbtn">
-            <i class="fi fi-rr-home icon"></i>
+            <i className="fi fi-rr-home icon"></i>
             <span>Home</span>
           </button>
         </NavLink>
         <NavLink to="/categories" className="respnav-item">
           <button className="respnavbtn">
-            <i class="fi fi-rr-apps-add icon"></i>
+            <i className="fi fi-rr-apps-add icon"></i>
             <span>Categories</span>
           </button>
         </NavLink>
         <NavLink to="/cart" className="respnav-item">
           <button className="respnavbtn">
-            <i class="fi fi-rr-bell icon"></i>
+            <i className="fi fi-rr-bell icon"></i>
             <span>Notification</span>
           </button>
         </NavLink>
         <NavLink to="/profile" className="respnav-item">
           <button className="respnavbtn">
-            <i class="fi fi-rr-user icon"></i>
+            <i className="fi fi-rr-user icon"></i>
             <span>Profile</span>
           </button>
         </NavLink>
