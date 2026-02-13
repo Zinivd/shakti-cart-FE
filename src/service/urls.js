160807606385089d698c06d50bfb744835d479ed
SheikShuaib <sheikshah@dreaminfinity.in>
2026-02-13 14:50:57 +0530

    url changes

--- src/service/urls.js
+++ src/service/urls.js
@@ -1,7 +1,7 @@
 import { placeOrder } from "./api";
 
-const BASE_URL = "http://127.0.0.1:8000"; 
-// const BASE_URL = "https://api-prod.shakticart.com"; 
+// const BASE_URL = "http://127.0.0.1:8000"; 
+const BASE_URL = "https://api-prod.shakticart.com"; 
 
 
 export const Urls = {
